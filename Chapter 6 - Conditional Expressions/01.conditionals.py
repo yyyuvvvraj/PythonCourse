b64e428272e6392f23ef31ecc9ae303aa63c0aa2
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-03-28 23:26:19 +0530

    Chapter 6 - Conditional Expressions

---
+++ Chapter 6 - Conditional Expressions/01.conditionals.py
@@ -0,0 +1,8 @@
+a = int(input("Enter your age : "))
+
+if(a > 18) :
+    print("You are above the age of consent")
+elif(a < 0) :
+    print("You are not born yet")
+else :
+    print("You are below the age of consent")
---
+++ Chapter 6 - Conditional Expressions/02.if_elif_else_ladder.py
@@ -0,0 +1,12 @@
+a = int(input("Enter your age : "))
+
+if(a > 18) :
+    print("You are above the age of consent")
+elif(a < 0) :
+    print("You are not born yet")
+elif(a == 0) : 
+    print("You are concieved but not born yet")
+else :
+    print("You are below the age of consent")
+
+print("This is the end of the program")
