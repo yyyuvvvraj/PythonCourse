2d10e97cbd6da4af85bbf9369f483905869f7458
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-03-28 16:45:35 +0530

    Problem Set

---
+++ Chapter-1/PS/Problem3.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Shlok and Sujal are my best friends I wish to travel with them in the entire world and have a lot of fun together.")
+engine.runAndWait()
---
+++ Chapter-1/PS/Problem4.py
@@ -0,0 +1,7 @@
+import os
+
+directory_path = 'C:/Users/yuvra/OneDrive - st.niituniversity.in/Personal Growth/Python/Chapter-1'
+
+contents = os.listdir(directory_path)
+
+print(contents)
