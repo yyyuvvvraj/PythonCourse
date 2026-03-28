460689cd98ede1bb08b52a996121b50187a3ce34
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-03-28 20:01:17 +0530

    Chapter-4

---
+++ Chapter-4/02.list_methods.py
@@ -0,0 +1,15 @@
+friends = ["Apple","Orange",5,345.06, False, "Aakash","Rohan"]
+friends.append("Yuvraj")
+print(friends)
+
+l1 = [1,34,62,2,6,11]
+l1.sort()
+print(l1)
+l1.reverse()
+print(l1)
+l1.insert(1,3)
+print(l1)
+l1.pop(0)
+print(l1)
+l1.remove(1)
+print(l1)
---
+++ Chapter-4/04.tuple_methods.py
@@ -0,0 +1,4 @@
+a = (1,2,3,4,5)
+
+no = a.count(3)
+print(no)
