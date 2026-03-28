0c21fc719b1d030287b9a9c00b1582f740f4ab83
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2026-03-28 22:37:31 +0530

    Chapter 5 - Dictionary & Sets

---
+++ Chapter 5 - Dictionary & Sets/01.dict.py
@@ -0,0 +1,10 @@
+marks = {
+    "Harry" : 100,
+    "Shubham" : 56,
+    "Rohan" : 23,
+    0 : "Yuvraj"
+}
+
+print(marks.items())
+print(marks.keys())
+print(marks.values())
---
+++ Chapter 5 - Dictionary & Sets/03.set_methods.py
@@ -0,0 +1,7 @@
+s = {1, 5, 32}
+
+print(s , type(s))
+s.add(256)
+print(s)
+s.remove(5)
+print(s)
